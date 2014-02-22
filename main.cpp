47aeca09d319a3d077f33b0b212e290ce2a8c111
Vitor Shade <vitor.shade@gmail.com>
2014-02-21 22:37:13 -0300

    -Commented SDL dependency and commented SDL window code.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
+//#include <iostream>
 
-#include <SDL/SDL.h>
+//#include <SDL/SDL.h>
 
-#include "gl_context.h"
-#include "shader_program.h"
+//#include "gl_context.h"
+//#include "shader_program.h"
 
-int kScreenWidth = 1024;
-int kScreenHeight = 768;
-int kScreenBpp = 32;
+//int kScreenWidth = 1024;
+//int kScreenHeight = 768;
+//int kScreenBpp = 32;
 
-bool InitSdl()
-{
-//  if( SDL_Init(SDL_INIT_EVERYTHING) < 0){
-//    return false;
-//  }
-//  if( SDL_SetVideoMode( kScreenWidth, kScreenHeight, kScreenBpp, SDL_OPENGL) == NULL ){
-//    return false;
-//  }
-//  SDL_WM_SetCaption("OpenGL Screen", NULL);
-}
+//bool InitSdl()
+//{
+////  if( SDL_Init(SDL_INIT_EVERYTHING) < 0){
+////    return false;
+////  }
+////  if( SDL_SetVideoMode( kScreenWidth, kScreenHeight, kScreenBpp, SDL_OPENGL) == NULL ){
+////    return false;
+////  }
+////  SDL_WM_SetCaption("OpenGL Screen", NULL);
+//}
 
-int main()
-{
-    InitSdl();
-    GLContext gl;
-    gl.InitGL();
+//int main()
+//{
+//    InitSdl();
+//    GLContext gl;
+//    gl.InitGL();
 
-    ShaderProgram sp;
-    sp.CompileShaderFromFile("/media/Vitor/Development/Projects/Github/misc-projects/glsl_cookbook/GLSL_GeneralProject/basic.vert", kVertexShader);
-    sp.GetActiveAttribsMap();
-    sp.GetActiveUniformsMap();
-    return 0;
-}
+//    ShaderProgram sp;
+//    sp.CompileShaderFromFile("/media/Vitor/Development/Projects/Github/misc-projects/glsl_cookbook/GLSL_GeneralProject/basic.vert", kVertexShader);
+//    sp.GetActiveAttribsMap();
+//    sp.GetActiveUniformsMap();
+//    return 0;
+//}
 
